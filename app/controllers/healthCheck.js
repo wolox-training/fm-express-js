dae4f41a509d57bc75bd1974188118aebd5f8c21
f-medina <f.medina@accenture.com>
2021-10-25 17:00:40 -0300

    showing random phrase

--- app/controllers/healthCheck.js
+++ app/controllers/healthCheck.js
@@ -1,1 +1,13 @@
+const axios = require('axios');
+
 exports.healthCheck = (_, res) => res.status(200).send({ uptime: process.uptime() });
+
+exports.getOnesentence = async (_, res) => {
+  const {
+    data: {
+      data: [{ quoteText, quoteAuthor }]
+    }
+  } = await axios.get('https://quote-garden.herokuapp.com/api/v3/quotes/random');
+
+  res.status(200).json({ sentence: quoteText, author: quoteAuthor });
+};
--- app/routes.js
+++ app/routes.js
@@ -1,8 +1,9 @@
 // const controller = require('./controllers/controller');
-const { healthCheck } = require('./controllers/healthCheck');
+const { healthCheck, getOnesentence } = require('./controllers/healthCheck');
 
 exports.init = app => {
   app.get('/health', healthCheck);
+  app.get('/new', getOnesentence);
   // app.get('/endpoint/get/path', [], controller.methodGET);
   // app.put('/endpoint/put/path', [], controller.methodPUT);
   // app.post('/endpoint/post/path', [], controller.methodPOST);

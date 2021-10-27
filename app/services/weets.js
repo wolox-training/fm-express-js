804e03a2b0c852e0d688bdfeb185614bf9bba7f2
f-medina <f.medina@accenture.com>
2021-10-27 16:27:09 -0300

    first model and migration

--- app/services/weets.js
+++ app/services/weets.js
@@ -2,12 +2,11 @@ const axios = require('axios');
 const { logger } = require('express-wolox-logger');
 const config = require('../../config').common.weet;
 
-module.exports.getRandomWeet = function getRandomWeet() {
-  return axios
+exports.getRandomWeet = () =>
+  axios
     .get(`${config.url}/random`)
     .then(res => res.data.data[0].quoteText)
     .catch(error => {
       logger.error(error);
       throw new Error('An error has occurred when trying to get a Weet');
     });
-};

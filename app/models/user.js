bb7f474fa5101fcd6da93c1a7b98e50e79bde551
f-medina <f.medina@accenture.com>
2021-11-01 08:14:53 -0300

    adding primary key attributes on model

--- app/models/user.js
+++ app/models/user.js
@@ -2,7 +2,7 @@ module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define(
     'user',
     {
-      id: { type: DataTypes.INTEGER },
+      id: { type: DataTypes.INTEGER, primaryKey: true, notNull: true, autoIncrement: true },
       name: { type: DataTypes.STRING },
       surname: { type: DataTypes.STRING },
       email: { type: DataTypes.STRING },
